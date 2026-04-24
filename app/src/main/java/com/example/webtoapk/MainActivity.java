15740e335eced8256bc6687e46023e5c9f1c6e6d
arbindsapu12 <gameg15822@gmail.com>
2026-04-24 18:52:48 +0530

    Add MainActivity to load a web page in WebView

---
+++ app/src/main/java/com/example/webtoapk/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.webtoapk;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = new WebView(this);
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.loadUrl("https://example.com");
+
+        setContentView(webView);
+    }
+}
