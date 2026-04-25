490763ca33f5a0ffa98efdedca9a9e6bb7800765
arbindsapu12 <gameg15822@gmail.com>
2026-04-25 22:07:22 +0530

    Replace WebView with TextView displaying 'APP STARTED'

--- app/src/main/java/com/example/webtoapk/MainActivity.java
+++ app/src/main/java/com/example/webtoapk/MainActivity.java
@@ -2,26 +2,15 @@ package com.example.webtoapk;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.webkit.WebSettings;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_main);
-
-        WebView webView = findViewById(R.id.webview);
-
-        WebSettings settings = webView.getSettings();
-        settings.setJavaScriptEnabled(true);
-        settings.setDomStorageEnabled(true);
-
-        webView.setWebViewClient(new WebViewClient());
-
-        webView.loadUrl("https://webtoapk.online");
+        TextView t = new TextView(this);
+        t.setText("APP STARTED");
+        setContentView(t);
     }
 }
