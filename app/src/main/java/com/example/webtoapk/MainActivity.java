753b67e7d62ef839227422c24bead51fd880bba7
arbindsapu12 <gameg15822@gmail.com>
2026-04-25 22:25:05 +0530

    Change background color to green on app start

--- app/src/main/java/com/example/webtoapk/MainActivity.java
+++ app/src/main/java/com/example/webtoapk/MainActivity.java
@@ -2,15 +2,16 @@ package com.example.webtoapk;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.graphics.Color;
+import android.view.View;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        TextView t = new TextView(this);
-        t.setText("APP STARTED");
-        setContentView(t);
+        View v = new View(this);
+        v.setBackgroundColor(Color.GREEN);
+        setContentView(v);
     }
 }
