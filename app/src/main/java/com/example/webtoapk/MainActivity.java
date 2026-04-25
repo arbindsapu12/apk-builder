db15a5665622521db8123dd0bfbd84b5398d5de3
arbindsapu12 <gameg15822@gmail.com>
2026-04-25 21:51:59 +0530

    Replace WebView with TextView displaying 'APP OK'

--- app/src/main/java/com/example/webtoapk/MainActivity.java
+++ app/src/main/java/com/example/webtoapk/MainActivity.java
@@ -2,25 +2,16 @@ package com.example.webtoapk;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.webkit.WebSettings;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        WebView webView = new WebView(this);
-        setContentView(webView);
-
-        WebSettings settings = webView.getSettings();
-        settings.setJavaScriptEnabled(true);
-        settings.setDomStorageEnabled(true);
-
-        webView.setWebViewClient(new WebViewClient());
-
-        webView.loadUrl("https://webtoapk.online");
+        TextView t = new TextView(this);
+        t.setText("APP OK");
+        t.setTextSize(24f);
+        setContentView(t);
     }
 }
