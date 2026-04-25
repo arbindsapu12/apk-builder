9223d3921631acc8a1ca45bc8031dd736e009a73
arbindsapu12 <gameg15822@gmail.com>
2026-04-25 20:20:56 +0530

    Replace WebView with TextView displaying 'APP WORKING'

--- app/src/main/java/com/example/webtoapk/MainActivity.java
+++ app/src/main/java/com/example/webtoapk/MainActivity.java
@@ -1,25 +1,14 @@
 import android.app.Activity;
 import android.os.Bundle;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.webkit.WebSettings;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        WebView webView = new WebView(this);
-        webView.setBackgroundColor(0xFFFFFFFF);
-        setContentView(webView);
-
-        WebSettings settings = webView.getSettings();
-        settings.setJavaScriptEnabled(true);
-        settings.setDomStorageEnabled(true);
-
-        webView.setWebViewClient(new WebViewClient());
-
-        webView.loadUrl("https://example.com"); // light site test
+        TextView text = new TextView(this);
+        text.setText("APP WORKING");
+        setContentView(text);
     }
 }
